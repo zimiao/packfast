b4385bd55607f017ff612002924c0e5f11229ed4
zzyrachel <zzy.rachel@gmail.com>
2026-02-15 21:21:57 -0800

    Initial Commit

---
+++ packfast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  packfast
+//
+//  Created by Ziyi Zhang on 2/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ packfast/packfastApp.swift
@@ -0,0 +1,17 @@
+//
+//  packfastApp.swift
+//  packfast
+//
+//  Created by Ziyi Zhang on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct packfastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
